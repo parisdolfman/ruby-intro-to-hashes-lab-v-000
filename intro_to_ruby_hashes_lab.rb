c091b6912f259b49c3efa2294b41717e5e88f8c1
Paris Dolfman <parisdolfman@gmail.com>
2018-06-21 15:18:05 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -3,16 +3,23 @@ def new_hash
 end
 
 def actor
- actor = {:name => "Dwayne The Rock Johnson"}
+ actor = {
+   :name => "Dwayne The Rock Johnson"
+ }
 end
 
 def monopoly
-	monopoly = {:railroads => {}}
+	monopoly = {
+    :railroads => {}
+  }
 
 end
 
 def monopoly_with_second_tier
-		monopoly = {:railroads => {:pieces => 4, :names => {}, :rent_in_dollars => {}}}
+		monopoly = {
+      :railroads => 
+      {:pieces => 4, :names => {}, :rent_in_dollars => {}}
+    }
 
 end
 
